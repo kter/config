8bc8a7601ea5ce90ab28c8a3842ceb36941b5b36
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-04-26 08:33:28 +0900

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,41 @@
+    1  sudo dnf install ibus-mozc
+    2  sudo dnf update
+    7  sudo dnf copr enable sneexy/python-validity
+    8  sudo dnf install open-fprintd fprintd-clients fprinted-clients-pam python3-validity
+    9  sudo dnf install open-fprintd fprintd-clients fprintd-clients-pam python3-validity
+   66  sudo dnf install graphviz
+   75  dnf search htop
+   76  dnf search atop
+  104  sudo dnf update
+  115  sudo dnf install atop htop iostat
+  144  sudo dnf install sar
+  197  sudo dnf -y install telnet
+  216  sudo dnf -y install nmap
+  267  sudo dnf install info -y
+  270  dnf whatprovides mesa-libGLU
+  272  sudo dnf install zlib-devel mesalibGLU
+  273  sudo dnf install zlib-devel mesa-libGLU
+  276  sudo dnf install zlib
+  277  sudo dnf install zlib-devel
+  281  sudo dnf --help
+  282  dnf --help
+  283  dnf upgrade --help
+  284  sudo dnf update
+  285  man dnf -g dist
+  288  sudo dnf upgrade --refresh
+  291  sudo dnf system-upgrade download --releasever=42
+  292  sudo dnf system-upgrade reboot
+  293  sudo dnf install rpmconf
+  308  sudo dnf install grub2-efi
+  309  sudo dnf install grub2-efi-x86
+  310  sudo dnf install grub2-efi-x86 shim-x64
+  311  sudo dnf install grub2-efi-x64
+  317  sudo dnf install remove-retired-packages
+  319  sudo dnf repoquery --duplicates
+  320  sudo dnf remove --duplicates
+  321  sudo dnf list --extras
+  322  sudo dnf remove $(sudo dnf repoquery --extras --exclude=kernel,kernel-\*,kmod-\*)
+  323  sudo dnf autoremove
+  374  sudo dnf install gnome-keyring seahorse
+  410  sudo dnf install fcitx5 fcitx5-mozc fcit5-configtool
+  411  sudo dnf install fcitx5 fcitx5-mozc fcitx5-configtool
